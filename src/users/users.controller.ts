8d59492c38dc7971cb155df4c5207cf300380284
Dilshad Matkulov <dilshadmatkulov@.gmail.com>
2024-04-27 13:50:25 +0600

    TRCKR-8.feat: added FileInterceptor, public folder for photos, changed url

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,18 +3,36 @@ import {
   Controller,
   Post,
   UploadedFile,
+  UseInterceptors,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import path from 'path';
+import { randomUUID } from 'crypto';
+
 import { UsersService } from './users.service';
 import { CreateUserDto } from './user-dto/create-user.dto';
 
-@Controller('admin/new-users')
+@Controller('staff')
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
 
-  @Post()
+  @Post('register-user')
   @UsePipes(new ValidationPipe())
+  @UseInterceptors(
+    FileInterceptor('photo', {
+      storage: diskStorage({
+        destination: './public/uploads/staff',
+        filename: (_req, file, cb) => {
+          const extension = path.extname(file.originalname);
+          const filename = path.join('artists', randomUUID() + extension);
+          cb(null, filename);
+        },
+      }),
+    }),
+  )
   createUser(
     @UploadedFile() file: Express.Multer.File,
     @Body() createUserDto: CreateUserDto,
